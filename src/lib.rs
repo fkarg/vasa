3031714f0120325322101851e87a098585d10e50
fkarg <f.karg10@gmail.com>
2020-03-10 19:07:33 +0100

    added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
 //* uncomment everything except current
+
+
+/// A doubly-Linked List implementation using indices in a Vector instead of actual pointers.
+///
+/// The implementation in `std::collections` is bound to be faster.
 pub mod linked_list;
+
+/// FIFO-Queue implementation using `Vec`.
+///
+/// This is version is not hand-optimized.
 pub mod fifo;
 
 // */
